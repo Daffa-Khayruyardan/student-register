6e2a061ba87ba93cc157701d2d664ec09f65e6c6
JustLittleCode150 <daffakhayru@gmail.com>
2021-12-14 10:13:01 +0700

    update gulp config file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 // import gulp tool packages
 const { src, dest, watch, series } = require('gulp');
 const path = require('path');
-const gulpSass = require('gulp-sass');
+const gulpSass = require('gulp-sass')(require('sass'));
 const gulpEjs = require('gulp-ejs');
 const gulpCleanCss = require('gulp-clean-css');
+const gulpJsMinify = require('gulp-minify');
 const browserSync = require('browser-sync').create();
 
 // make browser sync gulp task 
 function browserHost(cb) {
-    browsersync.init({
+    browserSync.init({
         server: {
-            baseDir: './src'
+            baseDir: 'src/'
         }
     }) ;
 
@@ -20,28 +21,43 @@ function browserHost(cb) {
 
 // make browser hot reload
 function browserReload(cb) {
-    browsersync.reload();
+    browserSync.reload();
 
     // make call back for this function
     cb();
 }
 
+// make ejs compile and minify task
+function ejsMinify() {
+    return src('src/views/*.ejs')
+        .pipe(gulpEjs('src/views/*.ejs'))
+        .pipe(dest('dist'));
+}
+
 // make sass compile and minify task
 function sassMinify() {
-    return src('./src/scss/main.scss')
-        .pipe(gulpSass)
+    return src('src/scss/*.scss')
+        .pipe(gulpSass())
         .pipe(gulpCleanCss())
-        .pipe(dest('public/style'));
+        .pipe(dest('dist'));
 }
 
 // make javascript compile and minify task
 function jsMinify() {
-    return src('./src/js/main.js')
-        .pipe()
-        .pipe(dest('public/scripts'))
+    return src('src/js/*.js')
+        .pipe(gulpJsMinify())
+        .pipe(dest('dist'));
+}
+
+// make watch task to all compile process
+function watchAll() {
+    watch(['src/scss/*.scss', 'src/js/*.js', 'src/views/*.ejs'], series(sassMinify, jsMinify, ejsMinify, browserReload));
 }
 
 // run all task in gulp
 exports.default = series(
-    
+    sassMinify,
+    jsMinify,
+    browserHost,
+    watchAll
 );
