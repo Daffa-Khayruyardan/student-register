4859a2f9628433147051af652774bc4a1cd613bf
JustLittleCode150 <daffakhayru@gmail.com>
2021-12-16 18:42:26 +0700

    update several changed in files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Simple Student Data 
+
+### GRANT ALL PRIVILEGES ON *.* TO 'username'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
--- db.js
+++ db.js
@@ -3,7 +3,24 @@ const mariadb = require('mariadb');
 const pool = mariadb.createPool({
     host: 'localhost',
     use: 'root',
-    passwor: 'daffa123',
-    connectionLimit: 2
+    password: '',
 });
 
+async function connectDB() {
+    let getPool;
+
+    try {
+        console.log('running');
+        getPool = await pool.getConnection();
+
+        const show = await getPool.query('show databases');
+        console.log(show);
+        
+    }catch(err) {
+        console.log(err);
+    }finally {
+        console.log('Process run sucessfully');
+    }
+}
+
+connectDB()
